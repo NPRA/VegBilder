2dc5f518b33fde0b38ca50d31c1406ca42acbbc5
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-09-03 09:54:16 +0200

    Create simple two-column layout in the app component
    Using placeholder images.

--- vegbilder-client/src/components/App.js
+++ vegbilder-client/src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Grid columns={2}>
+      <Grid.Column>
+        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+      </Grid.Column>
+    </Grid>
+  );
 }
 
 export default App;
