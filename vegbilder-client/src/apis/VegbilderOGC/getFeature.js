8d81e57ae6897ec4113ef8ec485f994c06e2a076
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-09-14 23:21:29 +0200

    Clicking on the map now draws a rectangle which shows the bounding box used for getting image points from the WFS service
    This is useful purely for development purposes. A sort of debugging feature.
    
    Also reduced the max number of image points retrieved.

--- vegbilder-client/src/apis/VegbilderOGC/getFeature.js
+++ vegbilder-client/src/apis/VegbilderOGC/getFeature.js
@@ -3,10 +3,10 @@ import vegbilderOGC from "./vegbilderOGC";
 const getFeature = async (latlng) => {
   const { lat, lng } = latlng;
   const bbox = {
-    west: lng - 0.01,
-    south: lat - 0.01,
-    east: lng + 0.01,
-    north: lat + 0.01,
+    west: lng - 0.0002,
+    south: lat - 0.0001,
+    east: lng + 0.0002,
+    north: lat + 0.0001,
   };
   const srsname = "urn:ogc:def:crs:EPSG::4326";
   const typename = "vegbilder_1_0:Vegbilder_2020";
@@ -18,7 +18,7 @@ const getFeature = async (latlng) => {
     typenames: typename,
     typename: typename,
     startindex: 0,
-    count: 10000,
+    count: 10,
     srsname: srsname,
     bbox: `${bbox.south},${bbox.west},${bbox.north},${bbox.east},${srsname}`,
     outputformat: "application/json",
--- vegbilder-client/src/components/SelectedImagePoint/SelectedImagePoint.js
+++ vegbilder-client/src/components/SelectedImagePoint/SelectedImagePoint.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Rectangle } from "react-leaflet";
 import { useLeafletMap } from "use-leaflet";
 
 import getFeature from "../../apis/VegbilderOGC/getFeature";
 
 const SelectedImagePoint = () => {
   const map = useLeafletMap();
-  map.on("click", async (event) => {
-    console.log(`Clicked on location ${event.latlng}`);
-    const featureInfo = await getFeature(event.latlng);
-    console.log(featureInfo);
-  });
+  const [bbox, setBbox] = useState(null);
 
-  return <div>SelectedImagePoint</div>;
+  useEffect(() => {
+    map.on("click", async (event) => {
+      console.log(`Clicked on location ${event.latlng}`);
+      const { lat, lng } = event.latlng;
+      setBbox({
+        west: lng - 0.0002,
+        south: lat - 0.0001,
+        east: lng + 0.0002,
+        north: lat + 0.0001,
+      });
+      const featureInfo = await getFeature(event.latlng);
+      console.log(featureInfo);
+    });
+    return () => {
+      map.off("click");
+    };
+  }, []);
+
+  const renderBbox = () => {
+    if (bbox) {
+      return (
+        <Rectangle
+          bounds={[
+            [bbox.south, bbox.west],
+            [bbox.north, bbox.east],
+          ]}
+        />
+      );
+    } else {
+      return <div></div>;
+    }
+  };
+
+  return renderBbox();
 };
 
 export default SelectedImagePoint;
