78bf672b64c2e1897ffe1e97be6ce9ab7959d094
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-09-23 12:59:11 +0200

    Fix a bug in the calculation of distance between two points
    The input to the Math.cos function should be in radians, not degrees. This bug led to erroneous distances in the y-direction.

--- vegbilder-client/src/utilities/latlngUtilities.js
+++ vegbilder-client/src/utilities/latlngUtilities.js
@@ -1,10 +1,12 @@
-const metersPerDegreeLat = 111111; // = 10^7 / 90. Approximate value stemming from the original definition of 1 meter as 1/10^7 of the total distance between from equator to the north pole along the meridian running through Paris. It is close enough for our purposes.
+import { degreesToRadians } from "./mathUtilities";
+
+const metersPerDegreeLat = 111111; // = 10^7 / 90. Approximate value stemming from the original definition of 1 meter as 1/10^7 of the total distance from the equator to the north pole along the meridian running through Paris. It is close enough for our purposes.
 
 const getDistanceInMetersBetween = (pointA, pointB) => {
   const dlat = Math.abs(pointA.lat - pointB.lat);
   const dlng = Math.abs(pointA.lng - pointB.lng);
   const dy = metersPerDegreeLat * dlat;
-  const dx = metersPerDegreeLat * Math.cos(pointA.lat) * dlng; // = (The length of a 1 degree arc along the circle of latitude running through pointA) * (the longitudinal angle between the two points). This formula is only valid when the two points are on roughly the same latitude.
+  const dx = metersPerDegreeLat * Math.cos(degreesToRadians(pointA.lat)) * dlng; // = (The length of a 1 degree arc along the circle of latitude running through pointA) * (the longitudinal angle between the two points). This formula is only valid when the two points are on roughly the same latitude.
   return Math.sqrt(dx * dx + dy * dy);
 };
 
---
+++ vegbilder-client/src/utilities/mathUtilities.js
@@ -0,0 +1,5 @@
+const degreesToRadians = (degrees) => {
+  return (degrees * Math.PI) / 180;
+};
+
+export { degreesToRadians };
