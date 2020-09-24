d5c2c5415d03d9bb3e81c79cd973f12398d8f6bf
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-09-24 14:16:49 +0200

    Add buttons which allow you to go forwards/backwards to the next/previous image points
    The array of image points appears to be presorted by date and time, so the order is correct, for the most part (see issue 3 below).
    
    Known issues:
    1. When you get near the end of a lane, you also (naturally) are near the end of the nearbyImagePointsOnSameRoadAndLane array, triggering a refetch of that. But when the array is updated, you are still near the end of it, since there were no more image points to get (end of the lane). Thus a new fetch is triggered, and we have an infinite loop.
    2. ImagePoints in roundabouts are skipped if the lane code (FELTKODE) in the roundabout is different from that of the lane you came from. A roundabout with one lane will have FELTKODE=1, so if you are coming into the roundabout from a lane with FELTKODE=2, you will skip the roundabout and continue in lane 2 on the other side.
    3. The ordering of image points by date and time is not always what we want, as the road was not necessarily photographed in one continuous drive. So sudden jumps to the wrong place may occur. To fix this, the meter value (and possible section numbers of the road) will have to contribute to the sorting.

--- vegbilder-client/src/components/ImageView/ImageView.js
+++ vegbilder-client/src/components/ImageView/ImageView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Image } from "semantic-ui-react";
+import { Image, Button } from "semantic-ui-react";
 
 import fetchNearbyImagePointsWhenNecessary from "./effects/fetchNearbyImagePointsWhenNecessary";
 
@@ -14,19 +14,60 @@ const ImageView = ({ currentImagePoint, setCurrentImagePoint }) => {
     setNearbyImagePointsOnSameRoadAndLane,
   ] = useState(null);
 
-  useEffect(
-    () =>
-      fetchNearbyImagePointsWhenNecessary(
-        currentImagePoint,
-        nearbyImagePointsOnSameRoadAndLane,
-        setNearbyImagePointsOnSameRoadAndLane
-      ),
-    [currentImagePoint, nearbyImagePointsOnSameRoadAndLane]
-  );
+  const [nextImagePoint, setNextImagePoint] = useState(null);
+  const [previousImagePoint, setPreviousImagePoint] = useState(null);
+
+  useEffect(() => {
+    const setNextAndPreviousImagePoints = () => {
+      if (currentImagePoint && nearbyImagePointsOnSameRoadAndLane) {
+        const currentIndex = nearbyImagePointsOnSameRoadAndLane.findIndex(
+          (ip) => ip.id === currentImagePoint.id
+        );
+        if (currentIndex === -1) {
+          setNextImagePoint(null);
+          setPreviousImagePoint(null);
+          return;
+        }
+        const nextIndex = currentIndex + 1;
+        const previousIndex = currentIndex - 1;
+
+        if (nextIndex >= nearbyImagePointsOnSameRoadAndLane.length) {
+          setNextImagePoint(null);
+        } else {
+          setNextImagePoint(nearbyImagePointsOnSameRoadAndLane[nextIndex]);
+        }
+
+        if (previousIndex < 0) {
+          setPreviousImagePoint(null);
+        } else {
+          setPreviousImagePoint(
+            nearbyImagePointsOnSameRoadAndLane[previousIndex]
+          );
+        }
+      }
+    };
+
+    fetchNearbyImagePointsWhenNecessary(
+      currentImagePoint,
+      nearbyImagePointsOnSameRoadAndLane,
+      setNearbyImagePointsOnSameRoadAndLane
+    );
+    setNextAndPreviousImagePoints();
+  }, [currentImagePoint, nearbyImagePointsOnSameRoadAndLane]);
+
+  const goToNextImagePoint = () => {
+    setCurrentImagePoint(nextImagePoint);
+  };
+
+  const goToPreviousImagePoint = () => {
+    setCurrentImagePoint(previousImagePoint);
+  };
 
   return (
     <div>
       <Image src={imageUrl} />
+      <Button onClick={goToPreviousImagePoint}>Bakover</Button>
+      <Button onClick={goToNextImagePoint}>Fremover</Button>
     </div>
   );
 };
