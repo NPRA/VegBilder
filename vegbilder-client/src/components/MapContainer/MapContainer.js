b22f66495ae10a1788ae7342c165126453636dcf
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-10-22 22:53:54 +0200

    Create MapContainer with Leaflet map and a tile layer with background map
    The nvdbcache background map is only available with crs EPSG:25833 (UTM zone 33N), which is not supported natively by Leaflet, so the proj4leaflet plugin must be used.
    
    Zoom levels and origin are documented here: https://nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer
    
    I could not use the makeStyles hook to style the Leaflet map (make the map fill the available space). The Map component from React-Leaflet creates a div with class "leaflet-container", but I could not apply styles to that class using the makeStyles hook (and specifying styles for leafletContainer). So I had to create a regular css file for this.

--- vegbilder-client/src/components/MapContainer/MapContainer.js
+++ vegbilder-client/src/components/MapContainer/MapContainer.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default function Map() {
-  return <div id="map">Map</div>;
+import "./MapContainer.css";
+import { crsUtm33N } from "./crs";
+
+export default function MapContainer() {
+  return (
+    <div id="map">
+      <Map
+        center={[63.415854, 10.394951]}
+        zoom={14}
+        crs={crsUtm33N}
+        minZoom={4}
+        maxZoom={16}
+      >
+        <TileLayer
+          url="https://m{s}-nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
+          attribution="© NVDB, Geovekst, kommunene og Open Street Map contributors (utenfor Norge)"
+          subdomains="123456789"
+        />
+      </Map>
+    </div>
+  );
 }
