66adecf021626f72459e0190db3fc4ac71a175d9
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-10-22 21:23:02 +0200

    Apply a margin to push content below the Header
    Since the AppBar inside the header is positioned "fixed", we need to add an invisible margin of the same height, so that the following content will not be partially hidden behind the Header.

--- vegbilder-client/src/components/Header.js
+++ vegbilder-client/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import { Toolbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
 
 export default function Header() {
+  const classes = useStyles();
   return (
-    <AppBar position="fixed" color="primary" elevation={0}>
-      <Toolbar>Vegbilder</Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="fixed" color="primary" elevation={0}>
+        <Toolbar>Vegbilder</Toolbar>
+      </AppBar>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
