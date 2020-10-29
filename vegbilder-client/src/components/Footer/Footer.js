556c2691dd857e345e6c1b16d0220ac782c84ce0
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-10-29 13:59:22 +0100

    Set correct background color in footer and add a border to separate the footer from the image container

--- vegbilder-client/src/components/Footer/Footer.js
+++ vegbilder-client/src/components/Footer/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
-import { green } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   footer: {
-    backgroundColor: "green",
+    backgroundColor: theme.palette.primary.main,
+    borderTop: `1px solid ${theme.palette.common.grayDark}`,
     width: "100vw",
     height: "10em",
   },
-});
+}));
 
 export default function Footer() {
   const classes = useStyles();
