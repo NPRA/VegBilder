ab09e1de8a9c90b74f21ff0c64ebc876d6986d97
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-10-29 13:08:27 +0100

    Better horizontal layout in the Header
    Using an empty placeholder item to the right which has width equal to that of the SVV logo, the Search component is now perfectly centered.

--- vegbilder-client/src/components/Header/Header.js
+++ vegbilder-client/src/components/Header/Header.js
@@ -8,7 +8,10 @@ import Search from "../Search/Search";
 const useStyles = makeStyles({
   logo: {
     padding: "0.5em",
-    height: "5em",
+    width: "9em",
+  },
+  rightItem: {
+    width: "10em",
   },
 });
 
@@ -17,11 +20,20 @@ export default function Header() {
   return (
     <AppBar position="static" color="primary" elevation={0}>
       <Toolbar>
-        <img src="images/svv-logo.svg" className={classes.logo} />
-
-        <Grid container direction="row" justify="center">
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Grid item>
+            <img src="images/svv-logo.svg" className={classes.logo} />
+          </Grid>
           <Grid item>
             <Search />
+          </Grid>
+          <Grid item>
+            <div className={classes.rightItem}></div>
           </Grid>
         </Grid>
       </Toolbar>
