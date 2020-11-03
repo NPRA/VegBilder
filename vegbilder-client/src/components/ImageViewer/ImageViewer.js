04755fcd70f9f6caed08d12210927064ead49e3d
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-11-03 15:40:35 +0100

    Add state and effect hooks to ImageViewer for keeping track of image points on the current road/lane
    We need to know the next and previous image points in order to go forwards and backwards, and to find those, we need a sorted list of image points for the current lane. For this purpose the concept of a road context is introduced, which encompasses VEGKATEGORI, VEGNUMMER, KRYSSDEL, SIDEANLEGGSDEL and FELTKODE. This is used in a filter which extracts the relevant image points from the currently loaded image points. We could have used these properties directly from currentImagePoint, but we do not want the filtering to happen whenever the currentImagePoint changes (which can be fairly often). The filtering should only be done whenever new image points have been loaded, or the road context has changed (user selected an image point in a different lane or on a different road altogether).

--- vegbilder-client/src/components/ImageViewer/ImageViewer.js
+++ vegbilder-client/src/components/ImageViewer/ImageViewer.js
@@ -1,6 +1,11 @@
-import React from "react";
-import { useCurrentImagePoint } from "../../contexts/CurrentImagePointContext";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import _ from "lodash";
+
+import { useCurrentImagePoint } from "../../contexts/CurrentImagePointContext";
+import { useLoadedImagePoints } from "../../contexts/LoadedImagePointsContext";
+import { useCurrentCoordinates } from "../../contexts/CurrentCoordinatesContext";
+import { isEvenNumber } from "../../utilities/mathUtilities";
 
 const useStyles = makeStyles((theme) => ({
   imageContainer: {
@@ -17,6 +22,80 @@ const useStyles = makeStyles((theme) => ({
 export default function ImageViewer() {
   const classes = useStyles();
   const { currentImagePoint } = useCurrentImagePoint();
+  const { loadedImagePoints } = useLoadedImagePoints();
+
+  const [nextImagePoint, setNextImagePoint] = useState(null);
+  const [previousImagePoint, setPreviousImagePoint] = useState(null);
+
+  const [currentRoadContext, setCurrentRoadContext] = useState(null);
+  const [currentLaneImagePoints, setCurrentLaneImagePoints] = useState([]);
+
+  // Set road context based on current image point
+  useEffect(() => {
+    if (currentImagePoint) {
+      setCurrentRoadContext({
+        vegkategori: currentImagePoint.properties.VEGKATEGORI,
+        vegnummer: currentImagePoint.properties.VEGNUMMER,
+        kryssdel: currentImagePoint.properties.KRYSSDEL,
+        sideanleggsdel: currentImagePoint.properties.SIDEANLEGGSDEL,
+        feltkode: currentImagePoint.properties.FELTKODE,
+      });
+    }
+  }, [currentImagePoint]);
+
+  // Get image points for current road context in correct order
+  useEffect(() => {
+    function getSortedImagePointsForCurrentRoadContext() {
+      const currentLaneImagePoints = loadedImagePoints.imagePoints.filter(
+        (ip) =>
+          ip.properties.VEGKATEGORI === currentRoadContext.vegkategori &&
+          ip.properties.VEGNUMMER === currentRoadContext.vegnummer &&
+          ip.properties.KRYSSDEL === currentRoadContext.kryssdel &&
+          ip.properties.SIDEANLEGGSDEL === currentRoadContext.sideanleggsdel &&
+          ip.properties.FELTKODE === currentRoadContext.feltkode
+      );
+      const primaryFeltkode = parseInt(currentRoadContext.feltkode[0], 10);
+      const sortOrder = isEvenNumber(primaryFeltkode) ? "desc" : "asc"; // Feltkode is odd in the metering direction and even in the opposite direction
+      return _.orderBy(
+        currentLaneImagePoints,
+        ["properties.STREKNING", "properties.DELSTREKNING", "properties.METER"],
+        [sortOrder, sortOrder, sortOrder]
+      );
+    }
+    if (loadedImagePoints && currentRoadContext) {
+      const sortedImagePointsForCurrentLane = getSortedImagePointsForCurrentRoadContext();
+      setCurrentLaneImagePoints(sortedImagePointsForCurrentLane);
+    }
+  }, [loadedImagePoints, currentRoadContext]);
+
+  // Set next and previous image points
+  useEffect(() => {
+    if (currentImagePoint && currentLaneImagePoints) {
+      const currentIndex = currentLaneImagePoints.findIndex(
+        (ip) => ip.id === currentImagePoint.id
+      );
+      if (currentIndex === -1) {
+        setNextImagePoint(null);
+        setPreviousImagePoint(null);
+        return;
+      }
+      const nextIndex = currentIndex + 1;
+      const previousIndex = currentIndex - 1;
+
+      if (nextIndex >= currentLaneImagePoints.length) {
+        setNextImagePoint(null);
+      } else {
+        setNextImagePoint(currentLaneImagePoints[nextIndex]);
+      }
+
+      if (previousIndex < 0) {
+        setPreviousImagePoint(null);
+      } else {
+        setPreviousImagePoint(currentLaneImagePoints[previousIndex]);
+      }
+    }
+  }, [currentImagePoint, currentLaneImagePoints]);
+
   return (
     <div className={classes.imageContainer}>
       {currentImagePoint ? (
