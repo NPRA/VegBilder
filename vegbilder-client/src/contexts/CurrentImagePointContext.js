8ea4667df46a39349ac16790d330319a48154ef7
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-12-11 00:16:52 +0100

    Reflect current image id in URL and initialize current image point based on image id from URL if present
    A known weakness with this approach: If you pan far enough away or zoom out so that loadedImagePoints no longer contains the image point with the current image id, and you copy the URL and paste it in a browser, no image point will be selected, since no image point with the requested id can be found. A fix for this might be to set the currentImagePoint to null as soon as it is no longer available in loadedImagePoints, but this might also be disturbing for users.
    
    Also need to check that the image id query param is valid before using it. Will do that next.

--- vegbilder-client/src/contexts/CurrentImagePointContext.js
+++ vegbilder-client/src/contexts/CurrentImagePointContext.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+
+import useQueryParamState from "../hooks/useQueryParamState";
+import { useLoadedImagePoints } from "./LoadedImagePointsContext";
 
 const CurrentImagePointContext = React.createContext();
 
@@ -13,12 +17,36 @@ function useCurrentImagePoint() {
 }
 
 function CurrentImagePointProvider(props) {
+  const { loadedImagePoints } = useLoadedImagePoints();
+  const [currentImageId, setCurrentImageId] = useQueryParamState(
+    "imageId",
+    "",
+    isValidImageId
+  );
   const [currentImagePoint, setCurrentImagePointInternal] = useState(null);
 
   function setCurrentImagePoint(imagePoint) {
     setCurrentImagePointInternal(imagePoint);
+    setCurrentImageId(imagePoint.id);
     console.log(imagePoint);
   }
+
+  /* Initialize current image point based on currentImageId (from query param) once loadedImagePoints
+   * has been populated. We have to find the actual image point with that id among the loaded image
+   * points.
+   */
+  useEffect(() => {
+    if (currentImagePoint == null && currentImageId !== "") {
+      const imagePoint = _.find(
+        loadedImagePoints.imagePoints,
+        (ip) => ip.id === currentImageId
+      );
+      if (imagePoint) {
+        setCurrentImagePointInternal(imagePoint);
+        setCurrentImageId(imagePoint.id);
+      }
+    }
+  }, [currentImageId, currentImagePoint, loadedImagePoints, setCurrentImageId]);
 
   return (
     <CurrentImagePointContext.Provider
@@ -26,6 +54,10 @@ function CurrentImagePointProvider(props) {
       {...props}
     />
   );
+}
+
+function isValidImageId(imageId) {
+  return true; // TODO: Replace with actual check
 }
 
 export { CurrentImagePointProvider, useCurrentImagePoint };
