1ad15e4c2b72b48fc77e58897eea82d58dff0915
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-12-13 20:15:53 +0100

    Configurable text for report email template
    Text which should be configurable by non-programmers will be placed in /config/text.js.

---
+++ vegbilder-client/src/configuration/text.js
@@ -0,0 +1,10 @@
+const reportEmailTemplateText = {
+  emailAddress: "trond.furstenberg@vegvesen.no",
+  subject: "Melding om feil på vegbilde med id",
+  body: {
+    line1: "Jeg vil melde fra om feil på følgende vegbilde:",
+    line2: "Beskrivelse: <Vennligst beskriv hva som er feil>",
+  },
+};
+
+export { reportEmailTemplateText };
--- vegbilder-client/src/utilities/mailtoUtilities.js
+++ vegbilder-client/src/utilities/mailtoUtilities.js
@@ -1,11 +1,12 @@
+import { reportEmailTemplateText } from "../configuration/text";
 import { getShareableUrlForImage } from "./urlUtilities";
 
 function createMailtoHrefForReporting(imagePoint) {
   const url = getShareableUrlForImage(imagePoint);
-  const email = "trond.furstenberg@vegvesen.no";
-  const subject = `Melding om feil på vegbilde med id '${imagePoint.id}'`;
-  const body = `Jeg vil melde fra om feil på følgende vegbilde: ${url} \r\n\r\nBeskrivelse: <Vennligst beskriv hva som er feil>`;
-  const mailto = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
+  const emailAddress = reportEmailTemplateText.emailAddress;
+  const subject = `${reportEmailTemplateText.subject} "${imagePoint.id}"`;
+  const body = `${reportEmailTemplateText.body.line1} ${url} \r\n\r\n${reportEmailTemplateText.body.line2}`;
+  const mailto = `mailto:${emailAddress}?subject=${subject}&body=${encodeURIComponent(
     body
   )}`;
   return mailto;
