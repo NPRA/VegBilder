98f6e2bee4d29c1801020829f928b0f95eae94f1
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-12-13 20:44:45 +0100

    Make splash text configurable
    Also added basic scroll functionality to the splash screen, in case the welcome text needs to be very long (which it should not).

--- vegbilder-client/src/components/Splash/Splash.js
+++ vegbilder-client/src/components/Splash/Splash.js
@@ -3,6 +3,7 @@ import { Checkbox, FormControlLabel, makeStyles } from "@material-ui/core";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 import CloseButton from "../CloseButton/CloseButton";
+import { splashScreenText } from "../../configuration/text";
 
 const useStyles = makeStyles((theme) => ({
   splash: {
@@ -11,6 +12,8 @@ const useStyles = makeStyles((theme) => ({
     top: "50%",
     left: "50%",
     width: "50rem",
+    maxHeight: "70vh",
+    overflowY: "auto",
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
   },
@@ -49,30 +52,10 @@ export default function Splash() {
       <div className={classes.splash}>
         <CloseButton onClick={closeSplash} />
         <div className={classes.content}>
-          <h1>Velkommen til Vegbilder</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu
-            neque eget eros interdum condimentum sed rutrum orci. Etiam cursus
-            ac leo luctus tincidunt. Nunc consequat pellentesque magna ut
-            fermentum. Etiam sit amet elementum tellus. Vestibulum ante ipsum
-            primis in faucibus orci luctus et ultrices posuere cubilia curae;
-            Curabitur facilisis sem quam, nec tempus justo tempus eu. Aenean
-            congue urna sed lorem dictum tempor. Sed eget accumsan velit, in
-            ultricies urna. Pellentesque eu risus non velit maximus rutrum.
-          </p>
-          <p>
-            Nulla viverra neque eget suscipit suscipit. Nam eget volutpat orci,
-            eu posuere nisl. Nunc malesuada, elit vel sagittis molestie, mi mi
-            rhoncus diam, quis volutpat turpis nibh sit amet quam. Nulla vitae
-            est ac enim vehicula bibendum. Maecenas sed ornare lectus. Curabitur
-            dapibus non orci id lacinia. Donec eu maximus eros. Maecenas
-            elementum varius bibendum. Pellentesque non nisi vel nisl convallis
-            volutpat. Nulla facilisi. Nullam ac eros et odio pulvinar posuere.
-            Proin commodo libero ut nunc maximus, eget semper neque fringilla.
-            Nam sit amet metus quam. Integer pretium ante et aliquet tincidunt.
-            Vestibulum sit amet lectus a diam sodales consequat. Morbi gravida
-            risus libero, sit amet tincidunt turpis ullamcorper et.
-          </p>
+          <h1>{splashScreenText.header}</h1>
+          {splashScreenText.paragraphs.map((paragraph) => (
+            <p>{paragraph}</p>
+          ))}
           <FormControlLabel
             control={
               <Checkbox
