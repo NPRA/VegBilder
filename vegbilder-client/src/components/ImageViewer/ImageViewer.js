9138cfcc50da29620c0af62a9e2c6c8d9dfb256d
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-12-14 13:23:35 +0100

    Add missing dependency to usEffect dependency array

--- vegbilder-client/src/components/ImageViewer/ImageViewer.js
+++ vegbilder-client/src/components/ImageViewer/ImageViewer.js
@@ -258,6 +258,7 @@ export default function ImageViewer({ exitImageView, showMessage }) {
     setCurrentCoordinates,
     setCurrentImagePoint,
     goToNearestImagePointInOppositeLane,
+    showMessage,
   ]);
 
   return (
