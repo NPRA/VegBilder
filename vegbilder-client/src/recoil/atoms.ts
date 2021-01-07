be88b6e4e58e8db5878bfbf96e854cfa06844f8a
iinus <iinusen7@gmail.com>
2021-01-07 14:14:39 +0100

    add type and atom for coordinates

--- vegbilder-client/src/recoil/atoms.ts
+++ vegbilder-client/src/recoil/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from 'recoil';
+import { ICoordinates } from './types';
 
 export const playVideoState = atom({
   key: 'playVideoState',
@@ -8,4 +9,9 @@ export const playVideoState = atom({
 export const timerState = atom({
   key: 'timerState',
   default: 2000, // ms
+});
+
+export const coordinatesState = atom<ICoordinates>({
+  key: 'cordinatesState',
+  default: { latlng: { lat: 65, lng: 15 }, zoom: 4 },
 });
