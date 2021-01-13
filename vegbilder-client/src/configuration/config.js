af848c240d92cd4ea9b208e231c013012498d830
iinus <iinusen7@gmail.com>
2021-01-13 20:06:06 +0100

    find years from ogc capabilities

--- vegbilder-client/src/configuration/config.js
+++ vegbilder-client/src/configuration/config.js
@@ -1,3 +1,4 @@
+import { getAvailableYears } from 'apis/VegbilderOGC/getAvailableYears';
 /* Set the years for which image points should be made available. Should be listed
  * in reverse chronological order (ie. most recent first).
  * The application will attempt to fetch data from the OGC service based on the
@@ -14,6 +15,21 @@ const availableYears = [2020, 2019];
  * be the default, then replace the following line with:
  * const defaultYear = 2019;
  */
+
+const regexp = RegExp(/20\d{2}/, 'g');
+
+const res = async () => {
+  const ogcCapabilities = await getAvailableYears();
+  console.log(ogcCapabilities.data);
+
+  const matches = Array.from(ogcCapabilities.data.matchAll(regexp), (m) => m[0]);
+  let unique = [...new Set(matches)];
+  console.log(unique);
+  return ogcCapabilities;
+};
+
+res();
+
 const defaultYear = availableYears[0];
 
 export { availableYears, defaultYear };
