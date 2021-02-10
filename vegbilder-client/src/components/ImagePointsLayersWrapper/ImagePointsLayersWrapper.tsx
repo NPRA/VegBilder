374f9b2e882f9af0c9915da8c63b3df8bfc53473
iinus <iinusen7@gmail.com>
2021-02-10 15:37:03 +0100

    return layers from function instead

--- vegbilder-client/src/components/ImagePointsLayersWrapper/ImagePointsLayersWrapper.tsx
+++ vegbilder-client/src/components/ImagePointsLayersWrapper/ImagePointsLayersWrapper.tsx
@@ -10,26 +10,20 @@ const ImagePointLayersWrapper = () => {
   const zoom = useLeafletZoom();
   const year = useRecoilValue(currentYearState);
 
+  const getLayers = () => {
+    if (zoom > 9) return year === 'Nyeste' ? `Vegbilder_${2020}` : `Vegbilder_${year}`; // det skal komme et annet kartlag her for nyeste (nå brukes 2020 som placeholder)
+    return year === 'Nyeste' ? `Vegbilder_oversikt_${2020}` : `Vegbilder_oversikt_${year}`;
+  };
+
   const renderImagePointsLayer = () => {
     if (zoom > 14 && year !== 'Nyeste') {
       return <ImagePointsLayer shouldUseMapBoundsAsTargetBbox={true} />;
-    } else if (year === 'Nyeste' || zoom > 9) {
-      return (
-        <WMSTileLayer
-          url="https://www.vegvesen.no/kart/ogc/vegbilder_1_0/ows"
-          attribution="<a href='https://www.vegvesen.no/'>Statens vegvesen</a>"
-          layers={year === 'Nyeste' ? `Vegbilder_${2020}` : `Vegbilder_${year}`} // det skal komme et annet kartlag her for nyeste (nå brukes 2020 som placeholder)
-          format="image/png"
-          transparent={true}
-          opacity={0.6}
-        />
-      );
     } else {
       return (
         <WMSTileLayer
           url="https://www.vegvesen.no/kart/ogc/vegbilder_1_0/ows"
           attribution="<a href='https://www.vegvesen.no/'>Statens vegvesen</a>"
-          layers={year === 'Nyeste' ? `Vegbilder_${2020}` : `Vegbilder_oversikt_${year}`}
+          layers={getLayers()}
           format="image/png"
           transparent={true}
           opacity={0.6}
