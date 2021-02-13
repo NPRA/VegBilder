050a1e67c0c8721cab3e11d49b0dbd0c3751a3c1
iinus <iinusen7@gmail.com>
2021-02-13 12:35:57 +0100

    add iframe with scheme to pageinfo

--- vegbilder-client/src/components/PageInformation/PageInformation.tsx
+++ vegbilder-client/src/components/PageInformation/PageInformation.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
-
 import CloseButton from 'components/CloseButton/CloseButton';
 import { informationText } from 'constants/text';
-import FeedbackLink from 'components/FeedbackLink/FeedbackLink';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   information: {
@@ -11,14 +9,15 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%)',
     top: '50%',
     left: '50%',
-    width: '50rem',
-    maxHeight: '70vh',
+    minWidth: '50rem',
+    maxHeight: '98vh',
     overflowY: 'auto',
-    zIndex: 100,
+    zIndex: 1000,
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
     border: `1px solid ${theme.palette.common.grayDark}`,
     borderRadius: '0.5rem',
+    padding: '0.2rem',
   },
   content: {
     margin: '2rem 2rem 0 2rem',
@@ -27,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: '1rem',
   },
   openEmailButton: {
+    position: 'sticky',
     border: 'none',
     background: 'inherit',
     color: 'inherit',
@@ -35,6 +35,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 'inherit',
     cursor: 'pointer',
     padding: 0,
+    marginBottom: '1rem',
   },
 }));
 
@@ -46,31 +47,59 @@ interface IInformationProps {
 
 const PageInformation = ({ setVisible, isOnboarding }: IInformationProps) => {
   const classes = useStyles(isOnboarding);
+  const [openForm, setOpenForm] = useState(false);
+
+  const FORM_LINK =
+    'https://forms.office.com/Pages/ResponsePage.aspx?id=VGmFOFXt90mL3XOP-7_TkIgX88vaXV9Notkd5xXWTp5UOVIzNlE0T1hMUVk0N1ZDSzVIMkcyTk84VCQlQCN0PWcu';
 
   return (
     <div className={isOnboarding ? '' : classes.information}>
       <CloseButton transparent={true} onClick={setVisible} />
       <div className={classes.content}>
-        <Typography variant="h2" className={classes.paragraphs}>
-          {' '}
-          {informationText.header}{' '}
-        </Typography>
-        <Typography variant="body1" className={classes.paragraphs}>
-          {' '}
-          {informationText.text}{' '}
-        </Typography>
-        <Typography variant="body1" className={classes.paragraphs}>
-          {' '}
-          {informationText.text2}{' '}
-        </Typography>
-        <FeedbackLink title={'Gi tilbakemelding'}></FeedbackLink>
-        <Typography variant="body1" className={classes.paragraphs}>
-          {informationText.contact}
-          {informationText.email}
-        </Typography>
-        <Typography variant="body1" className={classes.paragraphs}>
-          {informationText.versionNumber}
-        </Typography>
+        {!openForm ? (
+          <>
+            <Typography variant="h2" className={classes.paragraphs}>
+              {' '}
+              {informationText.header}{' '}
+            </Typography>
+            <Typography variant="body1" className={classes.paragraphs}>
+              {' '}
+              {informationText.text}{' '}
+            </Typography>
+            <Typography variant="body1" className={classes.paragraphs}>
+              {' '}
+              {informationText.text2}{' '}
+            </Typography>
+          </>
+        ) : null}
+        <>
+          <button className={classes.openEmailButton} onClick={() => setOpenForm(!openForm)}>
+            {openForm ? 'Lukk tilbakemeldingskjema' : 'Gi tilbakemelding'}
+          </button>
+          {openForm ? (
+            <iframe
+              title="tilbakemeldingsskjema"
+              src={FORM_LINK}
+              width="1000px"
+              height="1200px"
+              frameBorder="0"
+              marginWidth={0}
+              marginHeight={0}
+              style={{ border: 'none', maxWidth: '100%', maxHeight: '100vh' }}
+              allowFullScreen
+            />
+          ) : (
+            <>
+              <Typography variant="body1" className={classes.paragraphs}>
+                {informationText.contact}
+                {informationText.email}
+              </Typography>
+              <Typography variant="body1" className={classes.paragraphs}>
+                {informationText.versionNumber}
+              </Typography>
+            </>
+          )}
+        </>
       </div>
     </div>
   );
