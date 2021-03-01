c93dab1f87bcba2a132f843ccc7098399c412773
iinus <iinusen7@gmail.com>
2021-03-01 12:12:09 +0100

    add all speed signs

--- vegbilder-client/src/components/Icons/Icons.js
+++ vegbilder-client/src/components/Icons/Icons.js
@@ -20,7 +20,15 @@ import { ReactComponent as SelectIcon } from './selectedImage.svg';
 import { ReactComponent as InformIcon } from './Inform.svg';
 import { ReactComponent as DotsHorizontalSmallIcon } from './Dots-Horizontal-small.svg';
 import { ReactComponent as HistorySmallIcon } from './HistorySmall.svg';
-import { ReactComponent as EightySignIcon } from './sign_80.svg';
+import { ReactComponent as ThirthySignIcon } from './speedSigns/sign_30.svg';
+import { ReactComponent as FourtySignIcon } from './speedSigns/sign_40.svg';
+import { ReactComponent as FiftySignIcon } from './speedSigns/sign_50.svg';
+import { ReactComponent as SixtySignIcon } from './speedSigns/sign_60.svg';
+import { ReactComponent as SeventySignIcon } from './speedSigns/sign_70.svg';
+import { ReactComponent as EightySignIcon } from './speedSigns/sign_80.svg';
+import { ReactComponent as NighntySignIcon } from './speedSigns/sign_90.svg';
+import { ReactComponent as HundredSignIcon } from './speedSigns/sign_100.svg';
+import { ReactComponent as HundredTenSignIcon } from './speedSigns/sign_110.svg';
 
 export {
   ArrowDownIcon,
@@ -45,5 +53,13 @@ export {
   InformIcon,
   DotsHorizontalSmallIcon,
   HistorySmallIcon,
+  ThirthySignIcon,
+  FourtySignIcon,
+  FiftySignIcon,
+  SixtySignIcon,
+  SeventySignIcon,
   EightySignIcon,
+  NighntySignIcon,
+  HundredSignIcon,
+  HundredTenSignIcon,
 };
