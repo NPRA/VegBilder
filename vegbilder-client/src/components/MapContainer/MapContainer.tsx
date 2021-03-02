52d79d3037b8917c5cdafe05a030f329b232ee57
iinus <iinusen7@gmail.com>
2021-03-02 20:37:03 +0100

    dont use hook because it updates when coordinates change. Change cursor on map from what the user does

--- vegbilder-client/src/components/MapContainer/MapContainer.tsx
+++ vegbilder-client/src/components/MapContainer/MapContainer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { LeafletMouseEvent } from 'leaflet';
 
 import { crsUtm33N } from './crs';
@@ -11,6 +11,14 @@ import { useCurrentCoordinates } from 'contexts/CurrentCoordinatesContext';
 import { currentYearState } from 'recoil/atoms';
 import { useCurrentImagePoint } from 'contexts/CurrentImagePointContext';
 import useNearestImagePoint from 'hooks/useNearestImagepoint';
+import { useLoadedImagePoints } from 'contexts/LoadedImagePointsContext';
+import getImagePointsInTilesOverlappingBbox from 'apis/VegbilderOGC/getImagePointsInTilesOverlappingBbox';
+import { settings } from 'constants/constants';
+import useQueryParamState from 'hooks/useQueryParamState';
+import { availableYearsQuery } from 'recoil/selectors';
+import { ILatlng, IImagePoint } from 'types';
+import { findNearestImagePoint } from 'utilities/imagePointUtilities';
+import { createSquareBboxAroundPoint } from 'utilities/latlngUtilities';
 
 interface IMapContainerProps {
   showMessage: (message: string) => void;
@@ -18,12 +26,16 @@ interface IMapContainerProps {
 
 const MapContainer = ({ showMessage }: IMapContainerProps) => {
   const { currentCoordinates, setCurrentCoordinates } = useCurrentCoordinates();
-  const currentYear = useRecoilValue(currentYearState);
   const { setCurrentImagePoint } = useCurrentImagePoint();
-  const nearestImagePoint = useNearestImagePoint(
-    showMessage,
-    'Fant ingen bilder i nærheten av der du klikket. Prøv å klikke et annet sted.'
-  );
+  const [cursor, setCursor] = useState('pointer');
+  const [isFetching, setIsFetching] = useState(false);
+  const { loadedImagePoints, setLoadedImagePoints } = useLoadedImagePoints();
+  const [currentYear, setCurrentYear] = useRecoilState(currentYearState);
+  const availableYears = useRecoilValue(availableYearsQuery);
+  const [, setQueryParamYear] = useQueryParamState('year');
+
+  const [mouseMoved, setMouseMoved] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
 
   /* Fetch image points in new target area when the user clicks on the map. If we find an image, we set the year to the year where we found the image.
    */
@@ -34,19 +46,93 @@ const MapContainer = ({ showMessage }: IMapContainerProps) => {
       zoom = 15;
     }
     setCurrentCoordinates({ latlng: userClickedLatLng, zoom: zoom });
-    setCurrentImagePoint(nearestImagePoint);
+    fetchImagePointsFromNewestYearByLatLng(userClickedLatLng);
   };
+
+  const onMouseDown = (event: LeafletMouseEvent) => {
+    event.originalEvent.preventDefault();
+    setScrolling(true);
+    setMouseMoved(false);
+  };
+
+  const onMouseUp = (event: LeafletMouseEvent) => {
+    setScrolling(false);
+    setCursor('pointer');
+    if (!mouseMoved) {
+      handleClick(event);
+    }
+  };
+
+  const onMouseMove = (event: LeafletMouseEvent) => {
+    event.originalEvent.preventDefault();
+    setMouseMoved(true);
+    if (scrolling) {
+      setCursor('grabbing');
+    }
+  };
+
+  async function fetchImagePointsFromNewestYearByLatLng(latlng: ILatlng) {
+    if (isFetching) return;
+    if (!loadedImagePoints || currentYear === 'Nyeste') {
+      const targetBbox = createSquareBboxAroundPoint(latlng, settings.nyesteTargetBboxSize);
+      let foundImage = false;
+      for (const year of availableYears) {
+        const { imagePoints, expandedBbox } = await getImagePointsInTilesOverlappingBbox(
+          targetBbox,
+          year
+        );
+        if (imagePoints && imagePoints.length > 0) {
+          setLoadedImagePoints({
+            imagePoints: imagePoints,
+            bbox: expandedBbox,
+            year: year,
+          });
+          const nearestImagePoint = selectNearestImagePointToCoordinates(imagePoints, latlng);
+          setIsFetching(false);
+          if (nearestImagePoint) {
+            const year = nearestImagePoint.properties.AAR;
+            setCurrentImagePoint(nearestImagePoint);
+            setCurrentYear(year);
+            setQueryParamYear(year.toString());
+            showMessage(
+              `Setter årstallet til ${year}, som er det året med de nyeste bildene i området.`
+            );
+            foundImage = true;
+            break;
+          }
+        }
+      }
+      if (!foundImage) {
+        showMessage(
+          'Finner ingen bilder i nærheten av der du klikket. Prøv å klikke et annet sted.'
+        );
+      }
+    }
+  }
+
+  const selectNearestImagePointToCoordinates = useCallback(
+    (imagePoints: IImagePoint[], latlng) => {
+      if (!imagePoints || !imagePoints.length || !currentCoordinates) return;
+      const nearestImagePoint = findNearestImagePoint(imagePoints, latlng, 300);
+      if (nearestImagePoint) {
+        return nearestImagePoint;
+      }
+    },
+    [currentCoordinates]
+  );
 
   return (
     <Map
       center={currentCoordinates.latlng}
+      style={currentYear === 'Nyeste' ? { cursor: cursor } : {}}
       zoom={currentCoordinates.zoom}
       crs={crsUtm33N}
       minZoom={4}
       maxZoom={16}
       zoomControl={false}
-      onclick={handleClick}
-      style={currentYear === 'Nyeste' ? { cursor: 'pointer' } : {}}
+      onmousedown={onMouseDown}
+      onmousemove={onMouseMove}
+      onmouseup={onMouseUp}
       onViewportChanged={({ center, zoom }) => {
         if (center && zoom) {
           // Center and zoom is not defined immediately after rendering, for some reason, so the above if check is necessary. (Or the app would crash if you start dragging the map immediately after rendering.)
