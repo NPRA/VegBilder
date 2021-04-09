fb569d22a8bb46bb89a08ddc5b1256a067163797
iinus <iinusen7@gmail.com>
2021-04-09 15:05:32 +0200

    make a new back to map button

--- vegbilder-client/src/components/ImageView/SmallMapContainer/SmallMapContainer.tsx
+++ vegbilder-client/src/components/ImageView/SmallMapContainer/SmallMapContainer.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 
 import { crsUtm33N } from '../../MapContainer/crs';
 import ImagePointsLayer from 'components/ImagePointsLayer/ImagePointsLayer';
 import 'components/MapContainer/MapContainer.css';
-import { IconButton, Tooltip } from '@material-ui/core';
+import { IconButton, Tooltip, Typography } from '@material-ui/core';
 import { EnlargeIcon } from 'components/Icons/Icons';
 import { useRecoilState } from 'recoil';
 import { latLngZoomQueryParameterState } from 'recoil/selectors';
+import { ArrowBack, NoEncryption } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   minimap: {
     position: 'absolute',
     width: '18vw',
     height: '18vw',
-    left: '1.25rem',
-    top: '1.25rem',
-    border: `1px ${theme.palette.primary.main} solid`,
-    boxShadow: '1px 2px 2px 2px rgba(0, 0, 0, 0.4)',
+    left: '0.5rem',
+    top: '3.4rem',
+    boxShadow: '2px 7px 7px rgba(0, 0, 0, 0.35)',
+    borderRadius: '10px',
     zIndex: 5,
+  },
+  minimapHeader: {
+    position: 'absolute',
+    top: '1rem',
+    left: '0.5rem',
+    backgroundColor: theme.palette.common.grayDarker,
+    zIndex: 1000,
+    width: '18vw',
+    minHeight: '2rem',
+    color: theme.palette.common.grayMenuItems,
+    textAlign: 'center',
+    borderRadius: '10px',
+    display: 'flex',
+    cursor: 'pointer',
+    opacity: 0.9,
+    '&:hover': {
+      color: theme.palette.common.orangeDark,
+      backgroundColor: theme.palette.common.grayDark,
+      opacity: 'revert',
+      '& span': {
+        '& svg': {
+          '& path': {
+            fill: theme.palette.common.orangeDark,
+          },
+        },
+      },
+    },
   },
   minimapHidden: {
     display: 'none',
@@ -30,6 +58,13 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 1000,
     bottom: 2,
     right: 2,
+  },
+  arrowBack: {
+    margin: '0 0.375rem 0 1rem',
+    alignSelf: 'center',
+  },
+  backToText: {
+    alignSelf: 'center',
   },
 }));
 
@@ -44,35 +79,40 @@ const SmallMapContainer = ({ exitImageView }: ISmallMapContainerProps) => {
   const maxZoom = 16;
 
   return (
-    <div className={classes.minimap}>
-      <Map
-        center={currentCoordinates}
-        zoom={Math.max(currentCoordinates.zoom || 4, minZoom)}
-        crs={crsUtm33N}
-        minZoom={minZoom}
-        maxZoom={maxZoom}
-        zoomControl={false}
-        onViewportChanged={({ center, zoom }) => {
-          if (center && zoom) {
-            const latlng = { lat: center[0], lng: center[1] };
-            setCurrentCoordinates({ ...latlng, zoom: zoom });
-          }
-        }}
-        attributionControl={false}
-      >
-        <Tooltip title="Åpne kart">
-          <IconButton className={classes.enlargeButton} onClick={exitImageView}>
-            <EnlargeIcon />
-          </IconButton>
-        </Tooltip>
-        <TileLayer
-          url="https://services.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
-          attribution="© NVDB, Geovekst, kommunene og Open Street Map contributors (utenfor Norge)"
-          subdomains="123456789"
-        />
-        <ImagePointsLayer shouldUseMapBoundsAsTargetBbox={false} />
-      </Map>
-    </div>
+    <>
+      <div className={classes.minimapHeader} onClick={exitImageView}>
+        {' '}
+        <ArrowBack className={classes.arrowBack} />
+        <Typography variant="body1" className={classes.backToText}>
+          {' '}
+          Tilbake til kart{' '}
+        </Typography>{' '}
+      </div>
+      <div className={classes.minimap}>
+        <Map
+          center={currentCoordinates}
+          zoom={Math.max(currentCoordinates.zoom || 4, minZoom)}
+          crs={crsUtm33N}
+          minZoom={minZoom}
+          maxZoom={maxZoom}
+          zoomControl={false}
+          onViewportChanged={({ center, zoom }) => {
+            if (center && zoom) {
+              const latlng = { lat: center[0], lng: center[1] };
+              setCurrentCoordinates({ ...latlng, zoom: zoom });
+            }
+          }}
+          attributionControl={false}
+        >
+          <TileLayer
+            url="https://services.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
+            attribution="© NVDB, Geovekst, kommunene og Open Street Map contributors (utenfor Norge)"
+            subdomains="123456789"
+          />
+          <ImagePointsLayer shouldUseMapBoundsAsTargetBbox={false} />
+        </Map>
+      </div>
+    </>
   );
 };
 
