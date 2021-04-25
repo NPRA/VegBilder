3ec6970c5ace74031158d08356b4c584adf3d977
iinus <iinusen7@gmail.com>
2021-04-25 11:06:51 +0200

    scroll versionlog, but let header and tabs be sticky

--- vegbilder-client/src/components/Header/PageInformation/tabs/VersionLog.tsx
+++ vegbilder-client/src/components/Header/PageInformation/tabs/VersionLog.tsx
@@ -1,43 +1,31 @@
-import React, { useState } from 'react';
-import { Divider, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import { makeStyles, Typography } from '@material-ui/core';
 
-import PopUpWrapper from 'components/wrappers/PopUpWrapper';
-import FeedbackFormFrame from 'components/FeedbackFormFrame/FeedbackFormFrame';
-import { helpText } from 'constants/text';
-import { DotsHorizontalSmallIcon, HistorySmallIcon } from 'components/Icons/Icons';
-import { FEEDBACK_SCHEME_URL } from 'constants/urls';
-import { currentVersion, versionLog } from 'constants/versions';
+import { versionLog } from 'constants/versions';
 
 const useStyles = makeStyles((theme) => ({
-  content: {
-    margin: '2rem 2rem 1rem 2rem',
-  },
-  paragraphs: {
-    paddingBottom: '1rem',
-  },
-  openFeedbackScheme: {
-    position: 'sticky',
-    border: 'none',
-    background: 'inherit',
-    color: 'inherit',
-    borderBottom: `1px solid ${theme.palette.common.charcoalLighter}`,
-    fontFamily: 'inherit',
-    fontSize: 'inherit',
-    cursor: 'pointer',
-    padding: 0,
-    marginBottom: '1rem',
-  },
-  rigthLeftText: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  icon: {
-    width: '0.8rem',
-    height: '0.8rem',
-    margin: '0 0.1rem',
-  },
   bullets: {
     color: theme.palette.common.grayRegular,
+  },
+  scrollContainer: {
+    overflowY: 'auto',
+    padding: '1rem 0',
+    maxHeight: '60vh',
+    '&::-webkit-scrollbar': {
+      backgroundColor: theme.palette.common.grayDarker,
+      width: '1rem',
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: theme.palette.common.grayDarker,
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: theme.palette.common.grayRegular,
+      borderRadius: '1rem',
+      border: `4px solid ${theme.palette.common.grayDarker}`,
+    },
+    '&::-webkit-scrollbar-button': {
+      display: 'none',
+    },
   },
 }));
 
@@ -47,16 +35,18 @@ const VersionLog = () => {
   return (
     <>
       <Typography variant="h2"> Versjonslogg </Typography>
-      {Object.entries(versionLog).map(([versionNumber, changes]) => (
-        <>
-          <Typography variant="subtitle1"> {versionNumber} </Typography>
-          <ul className={classes.bullets}>
-            {changes.map((change) => (
-              <li>{change}</li>
-            ))}
-          </ul>
-        </>
-      ))}
+      <div className={classes.scrollContainer}>
+        {Object.entries(versionLog).map(([versionNumber, changes]) => (
+          <>
+            <Typography variant="subtitle1"> {versionNumber} </Typography>
+            <ul className={classes.bullets}>
+              {changes.map((change) => (
+                <li>{change}</li>
+              ))}
+            </ul>
+          </>
+        ))}
+      </div>
     </>
   );
 };
--- vegbilder-client/src/components/wrappers/PopUpWrapper.tsx
+++ vegbilder-client/src/components/wrappers/PopUpWrapper.tsx
@@ -4,20 +4,35 @@ import { makeStyles } from '@material-ui/core';
 import CloseButton from 'components/CloseButton/CloseButton';
 
 const useStyles = makeStyles((theme) => ({
-  information: {
+  popupWrapper: {
     position: 'absolute',
     transform: 'translate(-50%, -50%)',
     top: '50%',
     left: '50%',
     minWidth: '50rem',
-    maxHeight: '100vh',
+    maxHeight: '95vh',
     overflowY: 'auto',
     zIndex: 100000,
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    border: `1px solid ${theme.palette.common.grayDark}`,
-    borderRadius: '0.5rem',
+    borderRadius: '10px',
+    boxShadow: '2px 7px 7px rgba(0, 0, 0, 0.35)',
     padding: '0.2rem',
+    '&::-webkit-scrollbar': {
+      backgroundColor: theme.palette.common.grayDarker,
+      width: '1rem',
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: theme.palette.common.grayDarker,
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: theme.palette.common.grayRegular,
+      borderRadius: '1rem',
+      border: `4px solid ${theme.palette.common.grayDarker}`,
+    },
+    '&::-webkit-scrollbar-button': {
+      display: 'none',
+    },
   },
 }));
 
@@ -30,7 +45,7 @@ const PopUpWrapper = ({ setVisible, children }: IInformationProps) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.information}>
+    <div className={classes.popupWrapper}>
       <CloseButton transparent={true} onClick={setVisible} />
       {children}
     </div>
