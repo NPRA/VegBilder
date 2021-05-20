017477235aba6aa92b4304858d36faf0b580a169
iinus <iinusen7@gmail.com>
2021-05-20 15:10:18 +0200

    remove unused property isOnBoarding from PageInformation

--- vegbilder-client/src/components/PageInformation/PageInformation.tsx
+++ vegbilder-client/src/components/PageInformation/PageInformation.tsx
@@ -27,10 +27,9 @@ const useStyles = makeStyles(() => ({
 
 interface IInformationProps {
   setVisible: () => void;
-  isOnboarding?: boolean;
 }
 
-const PageInformation = ({ setVisible, isOnboarding }: IInformationProps) => {
+const PageInformation = ({ setVisible }: IInformationProps) => {
   const classes = useStyles();
   const [selectedTab, setSelectedTab] = useState('Om');
 
