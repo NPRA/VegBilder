0b076ed22636ae374e779557c0cbcc4f5235d581
Therese Hansen Federl <touthehan@vegvesen.no>
2021-09-20 09:56:06 +0200

    Hover effect on year selector for consistency

--- vegbilder-client/src/components/Header/YearSelector/YearSelector.tsx
+++ vegbilder-client/src/components/Header/YearSelector/YearSelector.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.common.grayRegular,
     width: '8rem',
     '&:hover': {
-      opacity: 0.9,
+      color: Theme.palette.common.orangeDark
     },
   },
   calendarIcon: {
