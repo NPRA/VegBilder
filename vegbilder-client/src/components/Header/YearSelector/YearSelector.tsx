6b1c12c79729e32851af3ab3bf3f22d12e21831a
Therese Federl <theresehfederl@gmail.com>
2021-12-02 10:54:13 +0100

    Remove unused import

--- vegbilder-client/src/components/Header/YearSelector/YearSelector.tsx
+++ vegbilder-client/src/components/Header/YearSelector/YearSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
